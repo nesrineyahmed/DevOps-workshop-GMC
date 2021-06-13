08d94b50d1143b45ad8e2423245fc6602904c06a
Nesrine Yahmed <nessrine.yahmed@gmail.com>
2021-06-13 14:10:39 +0100

    Update job.sh

--- job.sh
+++ job.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 ls
-#g++ generate.cpp -o generate
-#chmod u=x generate
-#./generate
-#g++ search.cpp -o search
-#chmod u=x search
-#./search
+g++ generate.cpp -o generate
+chmod u=x generate
+./generate
+g++ search.cpp -o search
+chmod u=x search
+./search
           
-#if cmp -s RES.txt OUT.txt ; then echo SUCESS ; else exit 1 ; fi
+if cmp -s RES.txt OUT.txt ; then echo SUCESS ; else exit 1 ; fi
