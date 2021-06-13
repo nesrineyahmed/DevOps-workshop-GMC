a7d8d0c4686ef60fe41b1bf26e44bae05177aecc
Nesrine Yahmed <nessrine.yahmed@gmail.com>
2021-06-13 10:44:13 +0100

    Create job.sh

---
+++ job.sh
@@ -0,0 +1,8 @@
+g++ generate.cpp -o generate
+chmod u=x generate
+./generate
+g++ search.cpp -o search
+chmod u=x search
+./search
+          
+if cmp -s RES.txt OUT.txt ; then echo SUCESS ; else exit 1 ; fi
